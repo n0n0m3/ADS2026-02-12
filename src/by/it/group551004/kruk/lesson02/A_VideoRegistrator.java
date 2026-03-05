83cf47d4719c3593c9606be7211d71a940b3ed0f
s-angrea <krukulia41@gmail.com>
2026-03-05 12:23:49 +0300

    Auto-merge PR #48
    * add files
    
    * donalltasks
    
    * Add lesson 2

--- src/by/it/group551004/kruk/lesson02/A_VideoRegistrator.java
+++ src/by/it/group551004/kruk/lesson02/A_VideoRegistrator.java
@@ -1,4 +1,4 @@
-package by.it.a_khmelev.lesson02;
+package by.it.group551004.kruk.lesson02;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/by/it/group551004/kruk/lesson02/C_GreedyKnapsack.java
+++ src/by/it/group551004/kruk/lesson02/C_GreedyKnapsack.java
@@ -1,4 +1,4 @@
-package by.it.a_khmelev.lesson02;
+package by.it.group551004.kruk.lesson02;
 /*
 Даны
 1) объем рюкзака 4
